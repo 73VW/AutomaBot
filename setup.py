e4d0095a3cf84256a53952dbcdc497ea79b0762e
mael.pedretti <mael.pedretti@hotmail.fr>
2017-06-05 12:16:34 +0200

    Updated autodeploy added tox updated setup.py and setup.cfg now every new commit on master should automatically make a new dev version

--- setup.py
+++ setup.py
@@ -18,10 +18,5 @@ setup(
         'pyfiglet>=0.7.5',
         'toml>=0.9.2'
     ),
-    extras_require={
-        'fast': ('cchardet', 'aiodns'),  # making it faster (recommended)
-        'qa': ('flake8', 'isort', 'pycodestyle', 'pydocstyle', 'rstcheck'),
-        'docs': ('Sphinx>=1.6.0', 'sphinxcontrib-trio')
-    },
     include_package_data=True
 )
