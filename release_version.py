017f284eccf9e22936361e8dc430fe97c2392487
mael.pedretti <mael.pedretti@hotmail.fr>
2017-06-05 02:45:09 +0200

    Still fixing deployement version

--- release_version.py
+++ release_version.py
@@ -1,7 +1,7 @@
 # This program is placed into the public domain.
-
 """
-Gets the current version number.
+Get the current version number.
+
 If in a git repository, it is the current git tag.
 Otherwise it is the one contained in the PKG-INFO file.
 To use this script, simply import it in your setup.py file
@@ -19,56 +19,22 @@ https://gist.github.com/pwithnall/7bc5f320b3bdf418265a
 
 __all__ = ('get_version')
 
-import os
-import re
 import subprocess
-from os.path import dirname, isdir, join
-
-version_re = re.compile('^Version: (.+)$', re.M)
 
 
 def get_version():
-    d = dirname(__file__)
-
-    if isdir(join(d, '.git')):# Get the version using "git describe".
-        # Get the version using "git describe".
-        cmd = 'git tag -l [0-9]* "https://github.com/73VW/AutomaBot"'.split()
-        try:
-            version = subprocess.check_output(cmd).decode().strip()
-        except subprocess.CalledProcessError as e:
-            print(e.output)
-            print('Unable to get version number from git tags')
-            exit(1)
-
-        print(str(version))
-        print("ok")
+    """Return last git tag."""
+    cmd = 'git tag -l [0-9]* "https://github.com/73VW/AutomaBot"'.split()
+    try:
+        version = subprocess.check_output(cmd).decode().strip()
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        print('Unable to get version number from git tags')
         exit(1)
 
-        # PEP 386 compatibility
-        if '-' in version:
-            version = '.post'.join(version.split('-')[:2])
-
-        # Don't declare a version "dirty" merely because a time stamp has
-        # changed. If it is dirty, append a ".dev1" suffix to indicate a
-        # development revision after the release.
-        with open(os.devnull, 'w') as fd_devnull:
-            subprocess.call(['git', 'status'],
-                            stdout=fd_devnull, stderr=fd_devnull)
-
-        cmd = 'git diff-index --name-only HEAD'.split()
-        try:
-            dirty = subprocess.check_output(cmd).decode().strip()
-        except subprocess.CalledProcessError:
-            print('Unable to get git index status')
-            exit(1)
-
-        if dirty != '':
-            version += '.dev1'
-
-    else:
-        # Extract the version from the PKG-INFO file.
-        with open(join(d, 'PKG-INFO')) as f:
-            version = version_re.search(f.read()).group(1)
+    print(str(version[0]))
+    print("ok")
+    exit(1)
 
     return version
 
