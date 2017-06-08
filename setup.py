79230cd88e92c444fbf518174502304987cddc12
mael.pedretti <mael.pedretti@hotmail.fr>
2017-06-08 09:05:18 +0200

    updated setup files and requirements cleaned setup.py changed home-page to github in setup.cfg let discord.py manage aiohttp in requirements.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 """Automabot bot for Discord."""
 
-from os import path
 from setuptools import setup, find_packages
 
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.rst'), 'r', encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     setup_requires=['pbr'],
